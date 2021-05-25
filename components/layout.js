0377e23b0c050eb958d63950693129fbef6e53d3
Casey Williams <caseyjw@mail.com>
2021-05-25 14:28:38 +0100

    Add profile pic again

--- components/layout.js
+++ components/layout.js
@@ -28,13 +28,29 @@ export default function Layout({ children, home }) {
       <header className={styles.header}>
         {home ? (
           <>
-          <h1>Yup</h1>
+            <Image
+              priority
+              src="/images/profile.jpg"
+              className={utilStyles.borderCircle}
+              height={144}
+              width={144}
+              alt={name}
+            />
             <h1 className={utilStyles.heading2Xl}>{name}</h1>
           </>
         ) : (
           <>
             <Link href="/">
-                <h1>Yup</h1>
+              <a>
+                <Image
+                  priority
+                  src="/images/profile.jpg"
+                  className={utilStyles.borderCircle}
+                  height={108}
+                  width={108}
+                  alt={name}
+                />
+              </a>
             </Link>
             <h2 className={utilStyles.headingLg}>
               <Link href="/">
